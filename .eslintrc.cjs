1180e79bb862d4589bd8649aa7fd74bd865ddb48
Pedro Martinho <pjemartinho@gmail.com>
2023-03-14 17:44:38 +0000

    new linter rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,8 +4,10 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   root: true,
   rules: {
-    indent: ["error", 2],
-    semi: ["error", "always"],
-    "max-len": ["error", { "code": 160 }]
-  }
+    indent: ['error', 2],
+    semi: ['error', 'always'],
+    'max-len': ['error', { 'code': 160 }],
+    'jsx-quotes': ['error', 'prefer-single'],
+    'no-trailing-spaces': 'error'
+  },
 };
