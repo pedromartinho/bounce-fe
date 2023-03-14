3e56c30f9a8b0c2241d1d50330af2d50a7c31541
Pedro Martinho <pjemartinho@gmail.com>
2023-03-14 09:36:06 +0000

    add linter commands and configs and add axios package

---
+++ .eslintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+  rules: {
+    indent: ["error", 2],
+    semi: ["error", "always"],
+    "max-len": ["error", { "code": 160 }]
+  }
+};
