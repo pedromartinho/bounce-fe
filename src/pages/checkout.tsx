00422364731f09c62ee889558d0b75efb65e7c62
Pedro Martinho <pjemartinho@gmail.com>
2023-03-14 10:49:11 +0000

    start communication with the BE

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -1,20 +1,32 @@
 import React, {useState} from 'react';
 import Counter from '../components/counter-input';
-import BlurMessage from '../components/loading';
+import BlurMessage from '../components/loading/loading';
 import TextInput from '../components/text-input';
+import { OrderService } from '../services/orders.service';
 
 function CheckoutPage() {
   const [success, setSuccess] = useState(false);
   const [loading, setLoading] = useState(false);
   const [failure, setFailure] = useState(false);
   const [name, setName] = useState("");
-  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
   const [cardNumber, setCardNumber] = useState("");
   const [numberOfBags, setNumberOfBags] = useState(1);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log({ name, phone, cardNumber });
+    const orderService = new OrderService();
+    setLoading(true);
+    orderService.create({
+      amount: numberOfBags,
+      unitPrice: 10, // TODO: Depend on the BE
+      name: name,
+      email,
+      cardNumber,
+    }).then(() => setSuccess(true))
+      .catch(() => setFailure(true))
+      .finally(() => setLoading(false));
+      
   };
 
   if (!success){
@@ -27,21 +39,19 @@ function CheckoutPage() {
         <hr/>
         <h2>Personal Details</h2>
         <TextInput label='Name' placeholder='John Doe' onChange={setName} />
-        <TextInput label='Phone' placeholder='john@doe.com' onChange={setPhone}/>
+        <TextInput label='Email' placeholder='john@doe.com' onChange={setEmail}/>
         <hr/>
         <h2>Payment Information</h2>
         <TextInput label='Card Details' placeholder='4242 4242 4242 4242' onChange={setCardNumber}/>
         {failure && <h2 onClick={() => setFailure(false)}>Your booking has failed. Please try again.</h2>}
         <h2>{`${numberOfBags} bags`}</h2>
         <button type="submit">Retry</button>
-        <button onClick={() => setSuccess(true)}>Request Success</button>
         <button onClick={() => setLoading(true)}>Loading</button>
-        <button onClick={() => setFailure(true)}>Request Failure</button>
         {loading && <BlurMessage message="Loading..." />}
       </form>
-    )
+    );
   } else {
-    return <h1 onClick={() => setSuccess(false)}> Booking Placed! </h1>
+    return <h1 onClick={() => setSuccess(false)}> Booking Placed! </h1>;
   }
 }
 
