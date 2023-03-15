83cfa6cff75de49462d088e3d593e02b8d208283
Pedro Martinho <pjemartinho@gmail.com>
2023-03-15 18:43:47 +0000

    text input styling

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -1,18 +1,20 @@
 import './TextInput.css';
 
 interface TextInputProps {
+  initialValue: string;
   label: string;
-  type?: 'text' | 'email | card';
+  type?: 'text' | 'email';
   placeholder: string;
   onChange: (value: string) => void;
 }
 
-const TextInput = ({label, type = 'text', placeholder, onChange}: TextInputProps) => {
+const TextInput = ({initialValue, label, type = 'text', placeholder, onChange}: TextInputProps) => {
   return (
     <div className='input-container'>
       <label className='input-label'>{label}</label>
       <input
         required
+        value={initialValue}
         className='input-field'
         type={type}
         id={label.toLowerCase()}
