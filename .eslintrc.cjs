5aa05c0d31be1a77b4302169c772b51802ead29a
Pedro Martinho <pjemartinho@gmail.com>
2023-03-15 18:40:05 +0000

    update on config files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,13 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  root: true,
-  rules: {
-    indent: ['error', 2],
-    semi: ['error', 'always'],
+  'extends': ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  'parser': '@typescript-eslint/parser',
+  'plugins': ['@typescript-eslint'],
+  'root': true,
+  'rules': {
+    'indent': ['error', 2],
+    'semi': ['error', 'always'],
     'max-len': ['error', { 'code': 160 }],
     'jsx-quotes': ['error', 'prefer-single'],
     'no-trailing-spaces': 'error'
-  },
+  }
 };
