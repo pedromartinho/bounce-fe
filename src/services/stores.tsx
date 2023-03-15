ae86a771d697a3769824465d477830871a4a3a41
Pedro Martinho <pjemartinho@gmail.com>
2023-03-15 18:47:40 +0000

    create store service

---
+++ src/services/stores.tsx
@@ -0,0 +1,31 @@
+import { env } from '../configs/env.config';
+import { ApiGatewayService } from './apiGateway';
+
+interface IStore {
+  name: string,
+  unitPrice: number,
+}
+/**
+ * A service responsible for handling store requests
+ * @class StoreService
+ */
+export class StoreService {
+  private storesEndpoint: string;
+  private apiGateway: ApiGatewayService;
+
+  constructor() {
+    this.apiGateway = new ApiGatewayService;
+    this.storesEndpoint = `${env.app.apiHost}/stores`;
+  }
+
+  /**
+   * Get store based on id
+   * @param {number} storeId - store id
+   * @returns {Promise<AxiosResponse>} - Request response
+   */
+  public async getStore(storeId: number): Promise<IStore> {
+    const storeUrl = `${this.storesEndpoint}/${storeId}`;
+    const {data} = await this.apiGateway.get(storeUrl);
+    return {name: data.name, unitPrice: data.unitPrice};
+  }
+}
