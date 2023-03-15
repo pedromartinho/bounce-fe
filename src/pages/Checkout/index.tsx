8f4ec59c7d1bd17041de3269f43534f436ac79f2
Pedro Martinho <pjemartinho@gmail.com>
2023-03-15 18:46:19 +0000

    checkout styling update

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -2,21 +2,53 @@ import React, {useState, useEffect} from 'react';
 import CounterInput from '../../components/CounterInput';
 import CreditCardInput from '../../components/CreditCardInput';
 import Loading from '../../components/Loading';
+import OverlayMessage from '../../components/OverlayMessage';
 import TextInput from '../../components/TextInput';
 import { OrderService } from '../../services/orders';
+import { StoreService } from '../../services/stores';
 import './Checkout.css';
 
-// If depends on store, should come from the
+// Leave just to have the page loaded even if BE fails
 const BAG_PRICE = 5.90;
 
 const CheckoutPage = () => {
+  const [unitPrice, setUnitPrice] = useState(BAG_PRICE);
+  const [storeName, setStoreName] = useState('A store');
   const [success, setSuccess] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [failure, setFailure] = useState(false);
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [cardNumber, setCardNumber] = useState("");
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [cardNumber, setCardNumber] = useState('');
   const [numberOfBags, setNumberOfBags] = useState(1);
+  const [validForm, setValidForm] = useState(false);
+
+  useEffect(() => {
+    const storeService = new StoreService();
+    storeService.getStore(1).then((data) => {
+      setUnitPrice(data.unitPrice);
+      setStoreName(data.name);
+    })
+      .catch(() => setFailure(true))
+      .finally(() => setLoading(false));
+    const savedName = localStorage.getItem('name');
+    if(savedName) setName(savedName);
+    const savedEmail = localStorage.getItem('email');
+    if(savedEmail) setEmail(savedEmail);
+    const cardNumber = localStorage.getItem('cardNumber');
+    if(cardNumber) setCardNumber(cardNumber);
+    const numberOfBags = localStorage.getItem('numberOfBags');
+    if(numberOfBags && Number(numberOfBags) !== 1) setNumberOfBags(Number(numberOfBags));
+    setValidForm(isFormValid());
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('name', name);
+    localStorage.setItem('email', email);
+    localStorage.setItem('cardNumber', cardNumber);
+    localStorage.setItem('numberOfBags', numberOfBags.toString());
+    setValidForm(isFormValid());
+  }, [name, email, cardNumber, numberOfBags]);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -24,53 +56,66 @@ const CheckoutPage = () => {
     setLoading(true);
     setFailure(false);
     orderService.create({
-      amount: numberOfBags,
+      numberOfBags,
       unitPrice: Math.round(numberOfBags * BAG_PRICE * 100) / 100,
       name: name,
       email,
-      cardNumber,
-    }).then(() => setSuccess(true))
+      creditCardNumber: cardNumber,
+    }).then(() => {
+      setSuccess(true);
+      localStorage.clear();
+    })
       .catch(() => setFailure(true))
       .finally(() => setLoading(false));
   };
 
+  const isFormValid = (): boolean => {
+    if (!name || !email || !cardNumber || !numberOfBags || cardNumber.length !== 19) {
+      return false;
+    }
+
+    return true;
+  };
+
   if (!success){
     return (
-      <form onSubmit={handleSubmit}>
+      <form className='top-section' onSubmit={handleSubmit}>
         <div className='checkout-section'>
-          <h4>Booking storage at:</h4>
-          <h2>Cody's Cookie Store</h2>
-          <CounterInput initialValue={numberOfBags} onChange={setNumberOfBags}/>
+          <p className='sub-title'>Booking storage at:</p>
+          <p className='title'>{storeName}</p>
+          <div className='counter-container'>
+            <p className='section-title'>Number of bags</p>
+            <CounterInput value={numberOfBags} setValue={setNumberOfBags}/>
+          </div>
         </div>
         <hr/>
         <div className='checkout-section'>
-          <h2>Personal Details</h2>
-          <TextInput label='Name' placeholder='John Doe' onChange={setName} />
-          <TextInput label='Email' placeholder='john@doe.com' onChange={setEmail}/>
+          <p className='section-title'>Personal Details:</p>
+          <TextInput initialValue={name} label='Name' type='text' placeholder='John Doe' onChange={setName} />
+          <TextInput initialValue={email} label='Email' type='email' placeholder='john@doe.com' onChange={setEmail}/>
         </div>
         <hr/>
         <div className='checkout-section'>
-          <h2>Payment Information</h2>
-          <CreditCardInput label='Card Details' placeholder='4242 4242 4242 4242' onChange={setCardNumber}/>
-          {failure && <h2 onClick={() => setFailure(false)}>Your booking has failed. Please try again.</h2>}
+          <p className='section-title'>Payment Information</p>
+          <CreditCardInput initialValue={cardNumber} label='Card Details' placeholder='4242 4242 4242 4242' onChange={setCardNumber}/>
+          {failure && <p className='error-message'>Your booking has failed.<br/> Please try again.</p>}
         </div>
         <div className='checkout-footer'>
-          <hr/>
-          <div className='checkout-section submit-form'>
+          <div className='submit-form'>
             <span>
-              <p>{`${numberOfBags} bags`}</p>
-              {/* TODO: Move to util or service file */}
-              <h2>{`$${Math.round(numberOfBags * BAG_PRICE * 100) / 100}`}</h2>
+              <p className='sub-text'>{`${numberOfBags} bags`}</p>
+              <p className='title'>{`$${Math.round(numberOfBags * unitPrice * 100) / 100}`}</p>
             </span>
-            <button className='checkout-button' type='submit'>Retry</button>
-            {/* <button className="checkout-button" onClick={() => setLoading(true)}>Loading</button> */}
+            <button style={{backgroundColor: failure ? '#F54B23': '#649DFF'}} className='submit-button' type='submit' disabled={!validForm}>
+              {failure ? 'Retry' : 'Book'}
+            </button>
           </div>
         </div>
         {loading && <Loading message='Loading...' />}
       </form>
     );
   } else {
-    return <h1 onClick={() => setSuccess(false)}> Booking Placed! </h1>;
+    return <OverlayMessage message='Booking Placed!' />;
   }
 };
 
